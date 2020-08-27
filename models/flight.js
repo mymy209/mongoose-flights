9413294047fc8625c537ed63ef60fd386ca29248
mymy209 <myramaronhime@gmail.com>
2020-08-27 14:54:08 -0700

    with required flightNo

--- models/flight.js
+++ models/flight.js
@@ -25,7 +25,8 @@ const flightSchema = new Schema ({
     flightNo: {
         type: Number, 
         min: 10, 
-        max: 9999
+        max: 9999,
+        required: true
     }, 
     departs: {
         type: Date, 
